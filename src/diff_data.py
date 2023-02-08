c7ae6f620d112ed7bcaa69f6c95578af7d088d6f
Pashkov K.V <pk69@kruger.su>
2023-02-08 17:59:11 +0700

    Разное

--- src/diff_data.py
+++ src/diff_data.py
@@ -1,14 +1,4 @@
 
-# curVal = ''
-# addinvent = ''
-
-# qrSelect_workshift = 'SELECT shiftnum ,cashcode  FROM workshift WHERE time_end >%s '
-# qrSelect_last_workshift_date = 'SELECT MAX(time_end) AS "MaxDate" FROM workshift'
-
-# qrSelect_workshift_open = 'SELECT shiftnum ,cashcode  FROM workshift WHERE time_end IS NULL AND time_beg >%s  '
-# qrSelect_last_workshift_date_open = 'SELECT MAX(time_beg) AS "MaxDate" FROM workshift Where time_end IS NULL'
-
-
 curVal = ''
 addinvent = ''
 
--- src/wsh_load.py
+++ src/wsh_load.py
@@ -16,7 +16,7 @@ from pathlib import Path
 logger = app_logger.get_logger(__name__)
 cPath = Path(os.getcwd())
 cPath.joinpath('Workshift_load', 'src')
-
+fileinit = '/home/administrator/Workshift_load/src/first.dat'
 sys.path.insert(1, '/home/administrator/Workshift_load/src/')
 """Для логирования событий"""
 
@@ -24,6 +24,12 @@ sys.path.insert(1, '/home/administrator/Workshift_load/src/')
 # Functions
 def main():
     """ Main program entry. """
+    # Если это первый запуск системы
+    if not os.path.exists(fileinit):
+        with open(fileinit, 'w', encoding='utf-8') as outfile:
+            outfile.write('')
+            init_pr()
+
     path = Path("config", "config.ini")
     logger.info("Start programs")
     logger.info("Current path " + str(cPath))
@@ -33,18 +39,19 @@ def main():
 
     # Список открытых смен от последнего зафиксированного времени
     l_workshift_open = tData.get_last_workshift_open()
-    # Если нечего отправлять, то и отправляем
+    logger.info(u'Number of open cash shifts - ' + str(len(l_workshift_open)))
+    # Если нечего отправлять, то и не отправляем
     if len(l_workshift_open) > 0:
-
         status_code = rec_con.post_workshift_open(l_workshift_open)
         # Меняем дату в файле только в случае успешного результата работы 1С
         if status_code == 200:
             tData.save_new_date_open()
         else:
-            logger.info(u'status_code_open - ' + str(status_code))
+            logger.warning('status_code_open - ' + str(status_code))
 
     # Список закрытых смен от последнего зафиксированного времени
     l_workshift = tData.get_last_workshift()
+    logger.info(u'Number of closed cash shifts - ' + str(len(l_workshift)))
     # Если нечего отправлять, то и отправляем
     if len(l_workshift) > 0:
         # rec_con = m_request.req1C(rc)
@@ -53,9 +60,25 @@ def main():
         if status_code == 200:
             tData.save_new_date()
         else:
-            logger.info(u'status_code - ' + str(status_code))
+            logger.warning('status_code - ' + str(status_code))
+    tData.close_db_connection()
 
-    logger.info(u'End programs')
+    logger.info('Normal shutdown of the program')
+    logger.info(
+        '******************************************************************************************************************')
+
+
+def init_pr():
+
+    filename = '/home/administrator/Workshift_load/src/last_date.txt'
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding='utf-8') as outfile:
+            outfile.write('2023-01-01 00:00:00')
+
+    filename = '/home/administrator/Workshift_load/src/last_date_open.txt'
+    if not os.path.exists(filename):
+        with open(filename, 'w', encoding='utf-8') as outfile:
+            outfile.write('2023-01-01 00:00:00')
 
 
 if __name__ == "__main__":
@@ -66,4 +89,5 @@ if __name__ == "__main__":
     if not rc == None:
         main()
     else:
-        logger.info(u'Программа завершила работу')
+        logger.error('Configuration file not found')
+        logger.info('The program has finished its work')
